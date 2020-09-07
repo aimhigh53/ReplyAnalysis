8a3f2bbceff6eeb2082f28e05e57d8e5c364f090
aimhigh53 <aimhigh53@gmail.com>
2020-09-07 23:41:59 +0900

    update

--- djangoProject/WebCrawlerApp/MongoDbManager.py
+++ djangoProject/WebCrawlerApp/MongoDbManager.py
@@ -1,10 +1,13 @@
+from pymongo import MongoClient
+from pymongo.cursor import CursorType
 import pymongo
+
 
 class MongoDbManager:
     _instance=None
-    client=pymongo.MongoClient(host='localhost',port=27017)
+    client=pymongo.MongoClient(host="localhost",port=27017)
     database = client['WebCrawlerDB']['WebCralwerCollection']
 
 
-###### method #####
 
+###### method #####
--- djangoProject/WebCrawlerApp/crawler.py
+++ djangoProject/WebCrawlerApp/crawler.py
@@ -16,7 +16,7 @@ import pandas as pd
 import numpy as np
 
 from WebCrawlerApp.chromedriver import generate_chrome
-
+from datetime import datetime, timedelta
 
 
 PROJECT_DIR = str(os.path.dirname(os.path.abspath(__file__)))
@@ -57,56 +57,82 @@ def loadPage():
     html = chrome.page_source
     soup = BeautifulSoup(html, 'lxml')
 
+def backToMainPage():
+    chrome.get(url)
 
 
-def getTopFive():
+def getTopFive(i):
     loadPage()
-    elements=soup.select('#wrap > table > tbody > tr > td.content > div > div:nth-child(5) > ol> li')
+    elements=soup.select('#wrap > table > tbody > tr > td.content > div > div:nth-child('+str(i)+') > ol> li')
+    titledic={}
 
     cnt=0
     for element in elements:
         cnt+=1
         title = element.select_one('dl > dt > a').text
         print('[',cnt,']번째 제목',title)
+
         if element.select_one('dl > dd > span.lede'):
-          print(element.select_one('dl > dd > span.lede').text)
+            title=title+element.select_one('dl > dd > span.lede').text
+            print(element.select_one('dl > dd > span.lede').text)
+
+        titledic[cnt] = title
+
+    return titledic
+
+
 #top5 댓글 가져오기
 
-def getReply():
-    clicknews=chrome.find_element_by_xpath('//*[@id="wrap"]/table/tbody/tr/td[2]/div/div[5]/ol/li[1]/dl/dt/a')
+def getReply(i,j):
+    replylist=[]
+    collecttime = str(datetime.utcnow().replace(microsecond=0) + timedelta(hours=9))[:16]
+    clicknews=chrome.find_element_by_xpath('//*[@id="wrap"]/table/tbody/tr/td[2]/div/div['+str(i)+']/ol/li['+str(j)+']/dl/dt/a')
     clicknews.click()
     time.sleep(1)
 
+    loadPage()
 
-    identify=chrome.find_element_by_xpath('//*[@id="cbox_module"]/div[2]/div[6]/div[1]/div/ul/li[1]/a/span[2]')
+    identify=chrome.find_element_by_xpath('//*[@id="cbox_module"]/div/div[6]/div[1]/div/ul/li[1]')
     identify.click()
     time.sleep(1)
 
-    more = chrome.find_element_by_xpath('//*[@id="cbox_module"]/div[2]/div[9]/a/span[1]')
+    more = chrome.find_element_by_xpath('//*[@id="cbox_module"]/div/div[9]/a/span[1]')
     more.click()
-    time.sleep(2)
+    time.sleep(1)
 
     contents = chrome.find_elements_by_css_selector('span.u_cbox_contents')
     likes = chrome.find_elements_by_css_selector('em.u_cbox_cnt_recomm')
     hates = chrome.find_elements_by_css_selector('em.u_cbox_cnt_unrecomm')
     cnt = 0
-    print(likes)
+
     for content,like,hate in zip(contents,likes,hates):
         cnt += 1
         print('[', cnt, ']번째')
         print(content.text,like.text,hate.text)
-
+        replylist.append(['[', cnt, ']번째',collecttime,content.text,like.text,hate.text])
+    return replylist
 
     # for reply in replys:
     #     replycontent = reply
     #     print(replycontent)
 
+    time.sleep(1)
 
-    time.sleep(3)
 
 
-getTopFive()
-getReply()
+for i in range(5,10):
+    titledic = getTopFive(i)
+    replylist=[]
+    for j in range(1,6):
+        replylist.append(getReply(i,j))
+        backToMainPage()
+
+    for index,title,replys in zip(titledic,titledic.values(),replylist):
+        print(index,title,replys)
+
+    #5개 top section, 5개 기사의 리플 가져오기
+
+
 
 
 
